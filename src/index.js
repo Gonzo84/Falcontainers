3a707281ffacb14658b0bc85b9929e8b81adaf5a
Marko Krstic <mkrstic@techyoo.de>
2023-04-20 23:01:25 +0200

    fixing navigation and removing markenidee section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import slick carousel
 import 'slick-carousel';
+
 require('jquery-ui/ui/widgets/dialog');
 // site config
 let headerHeight = 140;
@@ -20,9 +21,9 @@ const isFullySeen = el =>
 // on page ready
 $(document).ready(function () {
     // grt youtube
-    $.fn.grtyoutube = function( options ) {
+    $.fn.grtyoutube = function (options) {
 
-        return this.each(function() {
+        return this.each(function () {
 
             // Get video ID
             var getvideoid = $(this).attr("youtubeid");
@@ -32,20 +33,28 @@ $(document).ready(function () {
                 videoID: getvideoid,
                 autoPlay: true,
                 theme: "dark"
-            }, options );
+            }, options);
 
             // Convert some values
-            if(settings.autoPlay === true) { settings.autoPlay = 1 } else if(settings.autoPlay === false)  { settings.autoPlay = 0 }
-            if(settings.theme === "dark") { settings.theme = "grtyoutube-dark-theme" } else if(settings.theme === "light")  { settings.theme = "grtyoutube-light-theme" }
+            if (settings.autoPlay === true) {
+                settings.autoPlay = 1
+            } else if (settings.autoPlay === false) {
+                settings.autoPlay = 0
+            }
+            if (settings.theme === "dark") {
+                settings.theme = "grtyoutube-dark-theme"
+            } else if (settings.theme === "light") {
+                settings.theme = "grtyoutube-light-theme"
+            }
 
             // Initialize on click
-            if(getvideoid) {
-                $(this).on( "click", function() {
-                    $("body").append('<div class="grtyoutube-popup '+settings.theme+'">'+
-                        '<div class="grtyoutube-popup-content">'+
-                        '<span class="grtyoutube-popup-close"></span>'+
-                        '<iframe class="grtyoutube-iframe" src="https://www.youtube.com/embed/'+settings.videoID+'?rel=0&wmode=transparent&autoplay='+settings.autoPlay+'&iv_load_policy=3" allowfullscreen frameborder="0" allow="autoplay; fullscreen"></iframe>'+
-                        '</div>'+
+            if (getvideoid) {
+                $(this).on("click", function () {
+                    $("body").append('<div class="grtyoutube-popup ' + settings.theme + '">' +
+                        '<div class="grtyoutube-popup-content">' +
+                        '<span class="grtyoutube-popup-close"></span>' +
+                        '<iframe class="grtyoutube-iframe" src="https://www.youtube.com/embed/' + settings.videoID + '?rel=0&wmode=transparent&autoplay=' + settings.autoPlay + '&iv_load_policy=3" allowfullscreen frameborder="0" allow="autoplay; fullscreen"></iframe>' +
+                        '</div>' +
                         '</div>');
                 });
             }
@@ -53,44 +62,70 @@ $(document).ready(function () {
             // Close the box on click or escape
             $(this).on('click', function (event) {
                 event.preventDefault();
-                $(".grtyoutube-popup-close, .grtyoutube-popup").click(function(){
+                $(".grtyoutube-popup-close, .grtyoutube-popup").click(function () {
                     $(".grtyoutube-popup").remove();
                 });
             });
 
-            $(document).keyup(function(event) {
-                if (event.keyCode == 27){
+            $(document).keyup(function (event) {
+                if (event.keyCode == 27) {
                     $(".grtyoutube-popup").remove();
                 }
             });
         });
     };
 
-    $(".youtube-link").grtyoutube({autoPlay:true,theme: "dark"});
+    $(".youtube-link").grtyoutube({autoPlay: true, theme: "dark"});
     // initializing slider
-    $('.slider').slick({ dots: true });
+    $('.slider').slick({dots: true});
 
     // scrolling entire page to top on page load
-    $('html, body').animate({ scrollTop: $('#hero').offset().top }, lAnimationDuration);
-    setTimeout(function () { $('#hero').addClass('in-view'); }, lAnimationDuration);
+    $('html, body').animate({scrollTop: $('#hero').offset().top}, lAnimationDuration);
+    setTimeout(function () {
+        $('#hero').addClass('in-view');
+    }, lAnimationDuration);
+
+    const sections = document.getElementsByClassName("section");
+    function buildThresholdList() {
+        let thresholds = [];
+        let numSteps = 20;
+
+        for (let i = 1.0; i <= numSteps; i++) {
+            let ratio = i / numSteps;
+            thresholds.push(ratio);
+        }
+
+        thresholds.push(0);
+        return thresholds;
+    }
+    const observerOptions = {
+        root: null,
+        rootMargin: "0px",
+        threshold: buildThresholdList(),
+    };
+
+    const sectionsObserver = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(function (entry) {
+            const elementId = entry.target.id;
+            const $navItem = $("a." + elementId);
+            if (entry.isIntersecting && entry.intersectionRatio >= 0.51) {
+                $navItem.addClass('active');
+            } else {
+                $navItem.removeClass('active');
+            }
+        });
+    }, observerOptions);
+    Array.from(sections).forEach(function (section) {
+        sectionsObserver.observe(section);
+    });
 
     // on window scroll
     $(window).scroll(function () {
         // scrolling height from top
         const windscroll = $(window).scrollTop();
-        // hero section height
-        const localHeroSectionHeight = Math.round($('.section.hero').height() - headerHeight);
-
-        // setting active cls on header nav items while scrolling
-        $('.section:not(.vision)').each(function (i) {
-            if (Math.round($(this).position().top) <= windscroll + headerHeightOnClickNavigation) {
-                $('.nav-item:not(.sidemenu-list-item).active').removeClass('active');
-                $('.nav-item:not(.sidemenu-list-item)').eq(i).addClass('active');
-            }
-        });
 
         // setting css classes to header and sections that inform them that header height is reduced or not
-        if(windscroll >= 140) {
+        if (windscroll >= 140) {
             $('.header').addClass('header-reduced');
             $('.section').addClass('header-reduced');
 
@@ -100,7 +135,7 @@ $(document).ready(function () {
         }
 
         // iterating through every section
-        $('.section').each(function() {
+        $('.section').each(function () {
 
             /// scrolling on specific section result in giving that section in view section
             if (isFullySeen(this) === true) {
@@ -108,7 +143,7 @@ $(document).ready(function () {
             }
 
             // scrolling to the VISION section result in triggering animation
-            if(isFullySeen(this) === true && $(this).hasClass('vision')) {
+            if (isFullySeen(this) === true && $(this).hasClass('vision')) {
                 setTimeout(function () {
                     let myInterval = setInterval(function () {
                         if (twentyNumber < 20) {
@@ -119,7 +154,7 @@ $(document).ready(function () {
                             clearInterval(myInterval);
                         }
                     }, 150);
-                },1000);
+                }, 1000);
 
                 setTimeout(function () {
                     let myInterval2 = setInterval(function () {
@@ -144,39 +179,51 @@ $(document).ready(function () {
 
         if ($(this).hasClass('was-wir-tun')) {
             $('html, body').animate({scrollTop: Math.round($('#was-wir-tun').offset().top - headerHeightOnClickNavigation)}, lAnimationDuration);
-            setTimeout( () => {$('#was-wir-tun').addClass('in-view');}, lAnimationDuration);
-        }
-        if ($(this).hasClass('markenidee')) {
-            $('html, body').animate({scrollTop: Math.round($('#markenidee').offset().top - headerHeightOnClickNavigation)}, lAnimationDuration);
-            setTimeout(() => {$('#markenidee').addClass('in-view');}, lAnimationDuration);
+            setTimeout(() => {
+                $('#was-wir-tun').addClass('in-view');
+            }, lAnimationDuration);
         }
         if ($(this).hasClass('produkte')) {
             $('html, body').animate({scrollTop: Math.round($('#produkte').offset().top - headerHeightOnClickNavigation)}, lAnimationDuration);
-            setTimeout(() => {$('#produkte').addClass('in-view');}, lAnimationDuration);
+            setTimeout(() => {
+                $('#produkte').addClass('in-view');
+            }, lAnimationDuration);
         }
         if ($(this).hasClass('mission')) {
             $('html, body').animate({scrollTop: Math.round($('#mission').offset().top - headerHeightOnClickNavigation)}, lAnimationDuration);
-            setTimeout(() => {$('#mission').addClass('in-view');}, lAnimationDuration);
+            setTimeout(() => {
+                $('#mission').addClass('in-view');
+            }, lAnimationDuration);
         }
         if ($(this).hasClass('hintergrund')) {
             $('html, body').animate({scrollTop: Math.round($('#hintergrund').offset().top - headerHeightOnClickNavigation)}, lAnimationDuration);
-            setTimeout(() => {$('#hintergrund').addClass('in-view');}, lAnimationDuration);
+            setTimeout(() => {
+                $('#hintergrund').addClass('in-view');
+            }, lAnimationDuration);
         }
         if ($(this).hasClass('wertschopfung')) {
             $('html, body').animate({scrollTop: Math.round($('#wertschopfung').offset().top - headerHeightOnClickNavigation)}, lAnimationDuration);
-            setTimeout(() => {$('#wertschopfung').addClass('in-view');}, lAnimationDuration);
+            setTimeout(() => {
+                $('#wertschopfung').addClass('in-view');
+            }, lAnimationDuration);
         }
         if ($(this).hasClass('wir')) {
             $('html, body').animate({scrollTop: Math.round($('#wir').offset().top - headerHeightOnClickNavigation)}, lAnimationDuration);
-            setTimeout(() => {$('#wir').addClass('in-view');}, lAnimationDuration);
+            setTimeout(() => {
+                $('#wir').addClass('in-view');
+            }, lAnimationDuration);
         }
         if ($(this).hasClass('kontakt')) {
             $('html, body').animate({scrollTop: Math.round($('#kontakt').offset().top - headerHeightOnClickNavigation + 1)}, lAnimationDuration); // fix for not setting active cls on kontakt nav item
-            setTimeout(() => {$('#kontakt').addClass('in-view');}, lAnimationDuration);
+            setTimeout(() => {
+                $('#kontakt').addClass('in-view');
+            }, lAnimationDuration);
         }
         if ($(this).hasClass('faq')) {
             $('html, body').animate({scrollTop: Math.round($('#faq').offset().top - headerHeightOnClickNavigation + 1)}, lAnimationDuration); // fix for not setting active cls on faq nav item
-            setTimeout(() => {$('#faq').addClass('in-view');}, lAnimationDuration);
+            setTimeout(() => {
+                $('#faq').addClass('in-view');
+            }, lAnimationDuration);
         }
     });
 
@@ -211,7 +258,7 @@ $(document).ready(function () {
     }
 
     // on resize handler for function above
-    $(window).on('resize', function(){
+    $(window).on('resize', function () {
         checkPosition();
     });
 
@@ -222,8 +269,8 @@ $(document).ready(function () {
 
     var isMobile = false; //initiate as false
     // device detection
-    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|iP(hone|ad)|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
-        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
+    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|iP(hone|ad)|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
         isMobile = true;
     }
     !isMobile ? $('html').removeClass('on-mobile-device') : $('html').addClass('on-mobile-device');
@@ -321,20 +368,16 @@ var BrowserDetect = {
 
 BrowserDetect.init();
 
-var bv= BrowserDetect.browser;
+var bv = BrowserDetect.browser;
 
-if( bv == "Chrome"){
+if (bv == "Chrome") {
     $("body").addClass("chrome");
-}
-else if(bv == "Safari") {
+} else if (bv == "Safari") {
     $("body").addClass('safari')
-}
-else if(bv == "MS Edge"){
+} else if (bv == "MS Edge") {
     $("body").addClass("edge");
-}
-else if(bv == "Explorer"){
+} else if (bv == "Explorer") {
     $("body").addClass("ie");
-}
-else if(bv == "Firefox"){
+} else if (bv == "Firefox") {
     $("body").addClass("firefox");
 }
